37361cc6865c8e73c9735f76db7456f4a9dd9e8c
akillian@outlook.com <akillian@outlook.com>
2017-10-11 18:10:27 -0400

    Commit

--- server.js
+++ server.js
@@ -32,17 +32,21 @@ const authData = {
 // It may be repetitive to call this endpoint each time and not very performant, so you can cache the access_token once it is received.
 function getAccessToken(req, res, next) {
     request
-    .post('https://movieapi.auth0.com/oauth/token')
-    .send(authData)
-    .end(function(err, res) {
-        req.access_token = res.body.access_token;
-        next();
-      });
+        .post('https://movieapi.auth0.com/oauth/token')
+        .send(authData)
+        .end(function (err, res) {
+            if (req.body.access_token) {
+                req.access_token = res.body.access_token;
+                next();
+            } else {
+                res.send(401, 'Unauthorized');
+            }
+        });
 }
 
 // The homepage route of our application does not interface with the MovieAnalyst API and is always accessible. 
 // We won’t use the getAccessToken middleware here. We’ll simply render the index.ejs view.
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
     res.render('index');
 });
 
@@ -51,66 +55,66 @@ app.get('/', function(req, res) {
 // the request to our API.
 // Once the request is sent out, our API will validate that the access_token has the right scope to request the /movies resource and if 
 // it does, will return the movie data. We’ll take this movie data, and pass it alongside our movies.ejs template for rendering
-app.get('/movies', getAccessToken, function(req, res) {
+app.get('/movies', getAccessToken, function (req, res) {
     request
-       .get('http://localhost:8080/movies')
-       .set('Authorization', 'Bearer' + req.access_token)
-       .end(function(err, data) {
-           if(data.status == 403) {
-               res.send(403, '403 Forbidden');
-           } else {
-               const movies = data.body;
-               res.render('movies', { movies: movies});
-           }
-       });
+        .get('http://localhost:8080/movies')
+        .set('Authorization', 'Bearer' + req.access_token)
+        .end(function (err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                const movies = data.body;
+                res.render('movies', { movies: movies });
+            }
+        });
 });
 
 // The process will be the same for the remaining routes. We’ll make sure to get the acess_token first and then make the request to our 
 // API to get the data.The key difference on the authors route, is that for our client, we’re naming the route /authors, but our API 
 // endpoint is /reviewers. Our route on the client does not have to match the API endpoint route.
-app.get('/authors', getAccessToken, function(res, req) {
+app.get('/authors', getAccessToken, function (res, req) {
     request
-       .get('http://localhost:8080/reviewers')
-       .set('Authorization', 'Bearer' + req.access_token)
-       .end(function(err, data) {
-           if(data.status == 403) {
-               res.send(403, '403 Forbidden');
-           }  else {
-               const authors = data.body;
-               res.render('authors', {authors: authors});
-           }
-       });
+        .get('http://localhost:8080/reviewers')
+        .set('Authorization', 'Bearer' + req.access_token)
+        .end(function (err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                const authors = data.body;
+                res.render('authors', { authors: authors });
+            }
+        });
 });
 
-app.get('/publications', getAccessToken, function(req, res){
+app.get('/publications', getAccessToken, function (req, res) {
     request
-    .get('http://localhost:8080/publications')
-    .set('Authorization', 'Bearer' + req.access_token)
-    .end(function(err, data) {
-        if(data.status == 403) {
-            res.send(403,'403 Forbidden');
-        } else {
-            const publications = data.body;
-            res.render('publications', {publications: publications});
-        }
-    });
+        .get('http://localhost:8080/publications')
+        .set('Authorization', 'Bearer' + req.access_token)
+        .end(function (err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            } else {
+                const publications = data.body;
+                res.render('publications', { publications: publications });
+            }
+        });
 });
 
 // We’ve added the pending route, but calling this route from the MovieAnalyst Website will always result in a 403 Forbidden error as this 
 // client does not have the admin scope required to get the data.
-app.get('/pending', getAccessToken, function(req, res) {
+app.get('/pending', getAccessToken, function (req, res) {
     request
-       .get('http://localhost:8080/pending')
-       .set('Authorization', 'Bearer' + req.access_token)
-       .end(function(err, data) {
-           if(data.status == 403) {
-               res.send(403, '403 Forbidden');
-           }
-       });
+        .get('http://localhost:8080/pending')
+        .set('Authorization', 'Bearer' + req.access_token)
+        .end(function (err, data) {
+            if (data.status == 403) {
+                res.send(403, '403 Forbidden');
+            }
+        });
 });
 
 // Our MovieAnalyst Website will listen on port 3000. Feel free to change this as you see fit, just know that you can’t
 // have multiple processes listening on the same port.
-app.listen(3000, function() {
-    console.log('Express Server listening on port 3000');
+app.listen(3000, function () {
+    console.log('Express Server Web App listening on port 3000');
 });
